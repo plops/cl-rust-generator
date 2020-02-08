57ac7057389520b2cfeaca43b80e8f08a9f13adf
Martin Kielhorn <kielhorn.martin@gmail.com>
2020-02-08 15:07:18 +0100

    start with some simple code

--- examples/01_gcd/gen00.lisp
+++ examples/01_gcd/gen00.lisp
@@ -29,8 +29,26 @@
 
   (let ((code
 	 `(do0
-	    (defun main ()
-	      (declare (values int))))))
+	   "use std::io::Write"
+	   "use std::str::FromStr"
+	   
+	   (defun main ()
+	     (let ((numbers ("Vec::new")))
+	       (for (arg (dot ("std::env::args")
+			      (skip 1)))
+		    (numbers.push
+		     (dot ("u64::from_str" &arg)
+			  (expect (string "error parsing argument")))))
+	       (when (== 0 (numbers.len))
+		 (dot (writeln! ("std::io::stderr")
+				(string "Usage: gcd NUMBER ..."))
+		      (unwrap))
+		 ("std::process::exit" 1))
+	       (let ((d (aref numbers 0)))
+		 (for (m (ref (aref numbers "1..")))
+		      (setf d (gcd d *m))))
+	       (println! (string "The greatest common divisor of {:?} is {}"
+				 numbers d)))))))
     
     
     (write-source *code-file*
