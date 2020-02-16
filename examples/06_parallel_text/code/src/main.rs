b7d7467de48ad3a02d2f7ee219e014a4fb15ee29
Martin Kielhorn <kielhorn.martin@gmail.com>
2020-02-16 09:22:33 +0100

    start working on new example, also try to simplify use

---
+++ examples/06_parallel_text/code/src/main.rs
@@ -0,0 +1,2 @@
+use (std fs File)::(std io prelude *)::(std thread spawn)::(std sync mpsc
+                                                            channel);
---
+++ examples/06_parallel_text/gen00.lisp
@@ -0,0 +1,62 @@
+(eval-when (:compile-toplevel :execute :load-toplevel)
+  (ql:quickload "cl-rust-generator"))
+
+(in-package :cl-rust-generator)
+
+(declaim (optimize (speed 0)
+		   (safety 3)
+		   (debug 3)))
+
+;; cargo test
+;; cargo run
+;; cargo clean
+;; cargo fix
+
+;; https://github.com/ProgrammingRust/fingertips
+
+
+(progn
+  (defparameter *source-dir* #P"examples/06_parallel_text/code/src/")
+
+  (with-open-file (s (merge-pathnames #P"../Cargo.toml"
+				      *source-dir*)
+		     :direction :output
+		     :if-does-not-exist :create
+		     :if-exists :supersede)
+    (format s "~a"
+	    "[package]
+name = \"code\"
+version = \"0.1.0\"
+authors = [\"Martin Kielhorn <kielhorn.martin@gmail.com>\"]
+edition = \"2018\"
+
+# See more keys and their definitions at https://doc.rust-lang.org/cargo/reference/manifest.html
+
+[dependencies]
+argparse = \"*\"
+byteorder = \"*\"
+"
+
+
+	    ))
+  
+  (defparameter *code-file*
+    (asdf:system-relative-pathname 'cl-rust-generator
+				   (merge-pathnames #P"main.rs"
+						    *source-dir*)))
+
+  (let ((code
+	 `(do0
+	   (do0
+	    (use (std fs File)
+		 (std io prelude *)
+		 (std thread spawn)
+		 (std sync mpsc channel))
+	    )
+	   )))
+
+    
+    
+    (write-source *code-file*
+		  code)))
+
