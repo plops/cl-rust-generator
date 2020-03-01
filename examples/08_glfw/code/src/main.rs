26ca9cf308ee657d318f4cfdd825ffa0d03ab179
Martin Kielhorn <kielhorn.martin@gmail.com>
2020-03-01 19:16:47 +0100

    it compiles but complains at runtime
        Finished dev [unoptimized + debuginfo] target(s) in 0.01s
         Running `target/debug/code`
    code: third-party/cimgui/imgui/imgui.cpp:3518: void ImGui::NewFrame(): Assertion `g.IO.DisplaySize.x >= 0.0f && g.IO.DisplaySize.y >= 0.0f && "Invalid DisplaySize value!"' failed.
    Aborted

--- examples/08_glfw/code/src/main.rs
+++ examples/08_glfw/code/src/main.rs
@@ -1,42 +1,50 @@
 #[allow(unused_parens)]
+extern crate glfw;
+extern crate imgui;
+extern crate imgui_opengl_renderer;
 use glfw::{Action, Context, Key};
-fn main (){
-            let mut glfw  = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
+use std::ffi::CString;
+use std::os::raw::c_void;
+fn main() {
+    let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
     glfw.window_hint(glfw::WindowHint::ContextVersion(3, 3));
-        let (mut window, events)  = glfw.create_window(512, 512, "glfw win", glfw::WindowMode::Windowed).expect("failed to create glfw window");
+    let (mut window, events) = glfw
+        .create_window(512, 512, "glfw win", glfw::WindowMode::Windowed)
+        .expect("failed to create glfw window");
     window.make_current();
     window.set_key_polling(true);
-    gl::load_with(| symbol|{
-                return (window.get_proc_address(symbol) as *const);
-});
+    gl::load_with(|symbol| {
+        return window.get_proc_address(symbol);
+    });
     unsafe {
-                gl::Enable(gl::BLEND);
-                gl::BlendFunc(gl::SRC_ALPHA, gl::ONE_MINUS_SRC_ALPHA);
-                gl::Enable(gl::DEPTH_TEST);
-                gl::DepthFunc(gl::LESS);
-                gl::ClearColor(0.10    , 0.10    , 0.10    , 1.0    );
-}
-        let mut imgui  = imgui::Context::create();
-    let mut imgui_glfw  = imgui_glfw_rs::ImguiGLFW::new(&mut imgui, &mut window);
+        gl::Enable(gl::BLEND);
+        gl::BlendFunc(gl::SRC_ALPHA, gl::ONE_MINUS_SRC_ALPHA);
+        gl::Enable(gl::DEPTH_TEST);
+        gl::DepthFunc(gl::LESS);
+        gl::ClearColor(0.10, 0.10, 0.10, 1.0);
+    }
+    let mut imgui = imgui::Context::create();
+    imgui.set_ini_filename(None);
+    let renderer = imgui_opengl_renderer::Renderer::new(&mut imgui, |symbol| {
+        return window.get_proc_address(symbol);
+    });
     while (!(window.should_close())) {
-                unsafe {
-                        gl::Clear(((gl::COLOR_BUFFER_BIT) | (gl::DEPTH_BUFFER_BIT)));
-}
-                        let ui  = imgui_glfw.frame(&mut window, &mut imgui);
+        unsafe {
+            gl::Clear(((gl::COLOR_BUFFER_BIT) | (gl::DEPTH_BUFFER_BIT)));
+        }
+        let ui = imgui.frame();
         ui.show_demo_window(&mut true);
-        imgui_glfw.draw(ui, &mut window);
-                window.swap_buffers();
-                glfw.poll_events();
-                for  (_, event) in glfw::flush_messages(&events) {
-                        println!("{:?}", event);
-                        match event {
-                                glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => {
-                                window.set_should_close(true)
-},
-                                _ => {
-                                {}
-},
-}
-}
-};
+        renderer.render(ui);
+        window.swap_buffers();
+        glfw.poll_events();
+        for (_, event) in glfw::flush_messages(&events) {
+            println!("{:?}", event);
+            match event {
+                glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => {
+                    window.set_should_close(true)
+                }
+                _ => {}
+            }
+        }
+    }
 }
--- examples/08_glfw/gen00.lisp
+++ examples/08_glfw/gen00.lisp
@@ -11,7 +11,8 @@
   ;; chrono pulls in 9 more
   ;; gl pulls in 37-29=8
   ;; https://github.com/michaelfairley/rust-imgui-opengl-renderer
-;; https://docs.rs/imgui-glfw-rs/0.4.1/imgui_glfw_rs/
+  ;; https://docs.rs/imgui-glfw-rs/0.4.1/imgui_glfw_rs/
+  ;; https://github.com/michaelfairley/rust-imgui-sdl2/blob/master/examples/demo.rs
   (defun logprint (msg &optional (rest nil))
     `(progn
        (println! (string ,(format nil "{}:{} ~a ~{~a~^ ~}"
@@ -29,8 +30,8 @@
   (defparameter *module* nil)
   (defun define-module (args)
     (destructuring-bind (module-name module-code) args
-	(push `(:name ,module-name :code ,module-code)
-	      *module*)))
+      (push `(:name ,module-name :code ,module-code)
+	    *module*)))
 
 
 
@@ -52,89 +53,106 @@ authors = [\"Martin Kielhorn <kielhorn.martin@gmail.com>\"]
 
 [dependencies]
 #chrono = \"*\"
-#glfw = \"*\"
+glfw = \"*\"
 gl = \"*\"
-imgui-glfw-rs = \"*\"
-#imgui-rs = \"*\"
-#imgui_opengl_renderer = \"*\"
+#imgui-glfw-rs = \"*\"
+imgui = \"*\"
+imgui-opengl-renderer = \"*\"
 "
 
 
 	    ))
   (let ((screen-width 512)
 	(screen-height 512))
-   (define-module
-       `(main
-	 (do0
-	  ;;"extern crate glfw;"
-	  (use (glfw (curly Action Context Key)))
-	  (defun main ()
-	    (let* ((glfw (dot (glfw--init glfw--FAIL_ON_ERRORS)
-			      (unwrap))))
-	      (glfw.window_hint
-	       (glfw--WindowHint--ContextVersion 3 3))
-	     (let (
-		   ((values "mut window"
-			    events)
-		    (dot glfw
-			 (create_window ,screen-width
-					,screen-height
-					(string "glfw win")
-					glfw--WindowMode--Windowed)
-			 (expect (string "failed to create glfw window")))))
-	       (window.make_current)
-	       (window.set_key_polling true)
-	       (gl--load_with
-		(lambda (symbol)
-		  (return
-		    (coerce (dot window
-				(get_proc_address symbol))
-			   *const))
-		 ))
-	       (space unsafe
-		      (progn
-			(gl--Enable gl--BLEND)
-			(gl--BlendFunc gl--SRC_ALPHA gl--ONE_MINUS_SRC_ALPHA)
-			(gl--Enable gl--DEPTH_TEST)
-			(gl--DepthFunc gl--LESS)
-			(gl--ClearColor .1s0 .1s0 .1s0 1s0)))
-	       (let* ((imgui (imgui--Context--create))
-		      (imgui_glfw (imgui_glfw_rs--ImguiGLFW--new
-				   "&mut imgui"
-				   "&mut window"))))
-	       (while (not (window.should_close))
+    (define-module
+	`(main
+	  (do0
+	   "extern crate glfw;"
+	   "extern crate imgui;"
+	   "extern crate imgui_opengl_renderer;"
+	   (use (glfw (curly Action Context Key))
+		(std os raw c_void)
+		(std ffi CString))
+	       
+	   (defun main ()
+	     (let* ((glfw (dot (glfw--init glfw--FAIL_ON_ERRORS)
+			       (unwrap))))
+	       (glfw.window_hint
+		(glfw--WindowHint--ContextVersion 3 3))
+	       (let (
+		     ((values "mut window"
+			      events)
+		      (dot glfw
+			   (create_window ,screen-width
+					  ,screen-height
+					  (string "glfw win")
+					  glfw--WindowMode--Windowed)
+			   (expect (string "failed to create glfw window")))))
+		 (window.make_current)
+		 (window.set_key_polling true)
+		 (gl--load_with
+		  (lambda (symbol)
+		    (return (dot window
+				 (get_proc_address symbol))
+			    )
+		    
+		    ))
 		 (space unsafe
 			(progn
-			  (gl--Clear
-			   (logior gl--COLOR_BUFFER_BIT
-				   gl--DEPTH_BUFFER_BIT))))
-		 (let ((ui (imgui_glfw.frame "&mut window"
-					     "&mut imgui")))
-		   (ui.show_demo_window "&mut true")
-		   (imgui_glfw.draw ui "&mut window"))
-		 (window.swap_buffers)
-		 (glfw.poll_events)
-		 (for ((values _ event)
-		       (glfw--flush_messages &events))
+			  (gl--Enable gl--BLEND)
+			  (gl--BlendFunc gl--SRC_ALPHA gl--ONE_MINUS_SRC_ALPHA)
+			  (gl--Enable gl--DEPTH_TEST)
+			  (gl--DepthFunc gl--LESS)
+			  (gl--ClearColor .1s0 .1s0 .1s0 1s0)))
+		 (let* ((imgui (imgui--Context--create))
+			#+nil (imgui_glfw (imgui_glfw_rs--ImguiGLFW--new
+					   "&mut imgui"
+					   "&mut window")))
+		   (imgui.set_ini_filename None)
+		   (let ((renderer (imgui_opengl_renderer--Renderer--new
+				    "&mut imgui"
+				    (lambda (symbol)
+				      (return (dot  window
+						    (get_proc_address symbol))
+					      )
+				     
+		    ))))
+		    (while (not (window.should_close))
+		      (space unsafe
+			     (progn
+			       (gl--Clear
+				(logior gl--COLOR_BUFFER_BIT
+					gl--DEPTH_BUFFER_BIT))))
+		      (let ((ui (imgui.frame)))
+			(ui.show_demo_window "&mut true")
+		       (renderer.render ui))
+		      #+nil (let ((ui (imgui_glfw.frame "&mut window"
+						  "&mut imgui")))
+			(ui.show_demo_window "&mut true")
+			(imgui_glfw.draw ui "&mut window"))
+		      (window.swap_buffers)
+		      (glfw.poll_events)
+		      (for ((values _ event)
+			    (glfw--flush_messages &events))
 					;,(logprint "event" `(event))
-		      (println! (string "{:?}")
-				event)
-		      (case event
-			((glfw--WindowEvent--Key
-			  Key--Escape
-			  _
-			  Action--Press
-			  _)
-			 (window.set_should_close true))
-			(t "{}")))))))))))
+			   (println! (string "{:?}")
+				     event)
+			   (case event
+			     ((glfw--WindowEvent--Key
+			       Key--Escape
+			       _
+			       Action--Press
+			       _)
+			      (window.set_should_close true))
+			     (t "{}")))))))))))))
 
 
   (loop for e in (reverse *module*) and i from 0 do
-	   (destructuring-bind (&key name code) e
-	     (write-source (asdf:system-relative-pathname 'cl-rust-generator
-							  (merge-pathnames (format nil "~a.rs" name)
-									   *source-dir*))
-			   `(do0
-			     "#[allow(unused_parens)]"
-			     ;(use (chrono (curly DateTime Utc)))
-			     ,code)))))
+       (destructuring-bind (&key name code) e
+	 (write-source (asdf:system-relative-pathname 'cl-rust-generator
+						      (merge-pathnames (format nil "~a.rs" name)
+								       *source-dir*))
+		       `(do0
+			 "#[allow(unused_parens)]"
+					;(use (chrono (curly DateTime Utc)))
+			 ,code)))))
