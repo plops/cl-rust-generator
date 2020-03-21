af596945c510435e1d328af6a7426b92dd63e781
Martin Kielhorn <kielhorn.martin@gmail.com>
2020-03-21 22:21:52 +0100

    not sure why the shader isnt compiling

--- examples/13_vulkano/code/src/main.rs
+++ examples/13_vulkano/code/src/main.rs
@@ -37,15 +37,22 @@ fn main() {
         )
         .expect("failed to create buffer");
         mod cs {
-            vulkano_shaders::shader! {ty: "compute", src: r##"#version 450
+            vulkano_shaders::shader! {
+		ty: "compute",
+		src: "
+#version 450
+
 layout(local_size_x = 64, local_size_y = 1, local_size_z = 1) in;
-layout(set = 0, binding = 0) buffer Data { uint data[]; }
-buf;
+
+layout(set = 0, binding = 0) buffer Data { 
+  uint data[]; 
+} buf;
+
 void main() {
   uint idx = gl_GlobalInvocationID.x();
   buf.data[idx] = ((buf.data[idx]) * (12));
-}
-//"##}
+}"
+	    }
         }
         let shader = cs::Shader::load(device.clone()).expect("failed to create shader");
     }
