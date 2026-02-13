a7a86f5055be1b6d9cdba573092b0b5ddd6fbb47
wol <wolpumba@gmail.com>
2026-02-13 00:17:01 +0000

    fltk-rs example is 1.6mb

---
+++ examples/15_fltk/a_fltk/src/main.rs
@@ -0,0 +1,12 @@
+use fltk::{app, button::Button, frame::Frame, prelude::*, window::Window};
+
+fn main() {
+    let app = app::App::default();
+    let mut wind = Window::new(100, 100, 400, 300, "Hello from rust");
+    let mut frame = Frame::new(0, 0, 400, 200, "");
+    let mut but = Button::new(160, 210, 80, 40, "Click me!");
+    wind.end();
+    wind.show();
+    but.set_callback(move |_| frame.set_label("Hello World!"));
+    app.run().unwrap();
+}
