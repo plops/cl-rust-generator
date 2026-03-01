c2c7861937b0542376e16baba341608678f3f28c
wol <wolpumba@gmail.com>
2026-03-01 09:57:14 +0000

    delete

--- examples/20_webprox_avif/core-utils/src/lib.rs
+++
@@ -1,5 +0,0 @@
-pub struct ImageRgba {
-    pub width: u32,
-    pub height: u32,
-    pub pixels: Vec<u8>,
-}
--- examples/20_webprox_avif/proto-def/src/lib.rs
+++
@@ -1,5 +0,0 @@
-pub mod graphical_proxy {
-    tonic::include_proto!("graphical_proxy");
-}
-
-pub use graphical_proxy::*;
