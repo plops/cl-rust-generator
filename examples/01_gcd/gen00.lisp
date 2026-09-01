93a7df39eb48034888c4d25412fcdab7d81425cc
wol pumba <wolpumba@gmail.com>
2026-09-01 11:48:24 +0000

    more

--- examples/01_gcd/gen00.lisp
+++ examples/01_gcd/gen00.lisp
@@ -32,58 +32,45 @@
   (defparameter *source-dir* #P"examples/01_gcd/rs01_gcd/src/")
   (defparameter *code-file* (asdf:system-relative-pathname 'cl-rust-generator (merge-pathnames #P"main.rs"
 											       *source-dir*)))
+  (write-source
+   *code-file*
+   `(do0
+     "use std::str::FromStr;"
 
-  (let ((code
-	  `(do0
+     (defun gcd (n m)
+       (declare (type u64 n m)
+		(mutable n m)
+		(values u64))
+       (assert! (and (!= 0 n)
+		     (!= 0 m)))
+       (while (!= 0 m)
+	      (when (< m n)
+		(std--mem--swap "&mut m" "&mut n"))
+	      (%= m n))
+       n)
 
-	
+     "#[test]"
+     (defun test_gcd ()
+       (assert_eq! (gcd 14 15) 1)
+       (assert_eq! (gcd (* 2 3 5 11 17)
+			(* 3 7 11 13 19))
+		   (* 3 11)))
+     
+     (defun main ()
+       (let* ((numbers ("Vec::new")))
+	 (for (arg (dot ("std::env::args")
+			(skip 1)))
+	      (numbers.push
+	       (dot ("u64::from_str" &arg)
+		    (expect (string "error parsing argument")))))
+	 (when (numbers.is_empty)
+	   (eprintln!
+	    (string "Usage: gcd NUMBER ..."))
 	   
-	    "use std::str::FromStr;"
-
-	    (defun gcd (n m)
-	      (declare (type u64 n m)
-		       (mutable n m)
-		       (values u64))
-	      (assert! (and (!= 0 n)
-			    (!= 0 m)))
-	      (while (!= 0 m)
-		     (when (< m n)
-		       (std--mem--swap "&mut m" "&mut n")
-		       #+nil
-		       (let ((tt m))
-			 (declare (immutable tt))
-			 (setf m n
-			       n tt)))
-		     (%= m n)
-		     #+nil (setf m (% m n)))
-	      n)
-
-	    "#[test]"
-	    (defun test_gcd ()
-	      (assert_eq! (gcd 14 15) 1)
-	      (assert_eq! (gcd (* 2 3 5 11 17)
-			       (* 3 7 11 13 19))
-			  (* 3 11)))
-	   
-	    (defun main ()
-	      (let* ((numbers ("Vec::new")))
-		(for (arg (dot ("std::env::args")
-			       (skip 1)))
-		     (numbers.push
-		      (dot ("u64::from_str" &arg)
-			   (expect (string "error parsing argument")))))
-		(when (numbers.is_empty)
-		  (eprintln!
-			     (string "Usage: gcd NUMBER ..."))
-		  
-		  ("std::process::exit" 1))
-		(let* ((d (aref numbers 0)))
-		  (for (m (ref (aref numbers "1..")))
-		       (setf d (gcd d *m)))
-		  (println! (string "The greatest common divisor of {:?} is {}")
-			    numbers d)))))))
-    
-    
-    (write-source *code-file*
-		  code)))
+	   ("std::process::exit" 1))
+	 (let* ((d (aref numbers 0)))
+	   (for (m (ref (aref numbers "1..")))
+		(setf d (gcd d *m)))
+	   (println! (string "The greatest common divisor of {:?} is {}")
+		     numbers d)))))))
 
