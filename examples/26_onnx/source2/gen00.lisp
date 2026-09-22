cb54f1e1f0e9eee81300922ac634d416b10efaea
Wol Pumba <wolpumba@gmail.com>
2026-09-22 05:47:03 +0000

    fix some issues

--- examples/26_onnx/source2/gen00.lisp
+++ examples/26_onnx/source2/gen00.lisp
@@ -70,62 +70,67 @@
 
      ;; 4. Main application entry point
      (attr "macroquad::main(window_conf)"
-	   (defun-async main ()
-             (let (((paren conn screen) (dot (x11rb--connect None) (unwrap)))
-		   (root (dot (aref (dot conn (setup) roots) screen) root))
-		   "// Load the model directly from embedded memory"
-		   (session (dot (Session--builder)
-				 (unwrap)
-				 (commit_from_memory MODEL_BYTES)
-				 (unwrap)))
-		   (input_name (dot (aref (dot session (inputs)) 0) (name) (to_string)))
-		   (img (Image--gen_image_color (coerce SIZE u16) (coerce SIZE u16) BLACK))
-		   (tex (Texture2D--from_image (ref img)))
-		   (input (vec! (semicolon "0.0f32" (* 3 PLANE))))))
-             (declare (mutable session img input))
+       (defun-async main ()
+         (let (((paren conn screen) (dot (x11rb--connect None) (unwrap)))
+               (root (dot (aref (dot conn (setup) roots) screen) root))
+               (session (dot (Session--builder)
+                             (unwrap)
+                             (commit_from_memory MODEL_BYTES)
+                             (unwrap)))
+               (input_name (dot (aref (dot session (inputs)) 0) (name) (to_string)))
+               (img (Image--gen_image_color (coerce SIZE u16) (coerce SIZE u16) BLACK))
+               (tex (Texture2D--from_image (ref img)))
+               (input (vec! (semicolon "0.0f32" (* 3 PLANE)))))
+           (declare (mutable session)
+                    (mutable img)
+                    (mutable input))
 
-             (while (not (is_key_down KeyCode--Escape))
-		    (let ((reply (dot (xproto--get_image (ref conn)
-							 ImageFormat--Z_PIXMAP
-							 root
-							 0
-							 0
-							 (coerce SIZE u16)
-							 (coerce SIZE u16)
-							 u32--MAX)
-				      (unwrap)
-				      (reply)
-				      (unwrap)))
-			  ((paren r_plane rest) (dot input (split_at_mut PLANE)))
-			  ((paren g_plane b_plane) (dot rest (split_at_mut PLANE))))
+           (while (not (is_key_down KeyCode--Escape))
+             (let ((reply (dot (xproto--get_image (ref conn)
+                                                  ImageFormat--Z_PIXMAP
+                                                  root
+                                                  0
+                                                  0
+                                                  (coerce SIZE u16)
+                                                  (coerce SIZE u16)
+                                                  u32--MAX)
+                               (unwrap)
+                               (reply)
+                               (unwrap)))
+                   ((paren r_plane rest) (dot input (split_at_mut PLANE)))
+                   ((paren g_plane b_plane) (dot rest (split_at_mut PLANE))))
 
-		      ;; Copy BGRA window frame to RGB texture and planar model input buffer
-		      (for ((paren i px) (dot reply data (chunks_exact 4) (take PLANE) (enumerate)))
-			   (let (((paren b g r) (paren (aref px 0) (aref px 1) (aref px 2)))
-				 (off (* i 4)))
-			     (dot (aref (dot img bytes) (range off (+ off 4)))
-				  (copy_from_slice (ref (list r g b 255))))
-			     ,@(emit-pixel-planes 'i '(r_plane r) '(g_plane g) '(b_plane b))))
+               ;; Copy BGRA window frame to RGB texture and planar model input buffer
+               (for ((paren i px) (dot reply data (chunks_exact 4) (take PLANE) (enumerate)))
+                 (let (((paren b g r) (paren (aref px 0) (aref px 1) (aref px 2)))
+                       (off (* i 4)))
+                   (dot (aref (dot img bytes) "off..off + 4")
+                        (copy_from_slice (ref (list r g b 255))))
+                   ,@(emit-pixel-planes 'i '(r_plane r) '(g_plane g) '(b_plane b))))
 
-		      ;; Run inference on embedded ONNX session
-		      (let ((outputs (dot session
-					  (run (aref inputs!
-						     (space (dot input_name (as_str))
-							    "=>"
-							    (dot (TensorRef--from_array_view
-								  (paren (list 1 3 SIZE SIZE)
-									 (ref (aref input (range-full)))))
-								 (unwrap)))))
-					  (unwrap)))
-			    ((paren _ dets) (dot (aref outputs 0)
-						 ("try_extract_tensor::<f32>")
-						 (unwrap))))
+               ;; Run inference on embedded ONNX session
+               (let ((outputs (dot session
+                                   (run (aref inputs!
+                                              (space (dot input_name (as_str))
+                                                     "=>"
+                                                     (dot (TensorRef--from_array_view
+                                                           (paren (list 1 3 SIZE SIZE)
+                                                                  (ref (aref input ".."))))
+                                                          (unwrap)))))
+                                   (unwrap)))
+                     ((paren _ dets) (dot (aref outputs 0)
+                                          ("try_extract_tensor::<f32>")
+                                          (unwrap))))
 
-			(dot tex (update (ref img)))
-			(clear_background BLACK)
-			(draw_texture (ref tex) 0.0 0.0 WHITE)
+                 (dot tex (update (ref img)))
+                 (clear_background BLACK)
+                 (draw_texture (ref tex) 0.0 0.0 WHITE)
 
-			(for (det (dot dets (chunks_exact 6)))
-			     ,(emit-draw-detection 'det))
+                 (for (det (dot dets (chunks_exact 6)))
+                   ,(emit-draw-detection 'det))
 
-			(await (next_frame))))))))))
+                 (await (next_frame))))))))))
+
+  )
+
+
